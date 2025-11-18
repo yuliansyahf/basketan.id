043351a4bc9e17848facc258f185bf8002278f33
yuliansyah <yuliansyahfadi@gmail.com>
2025-11-18 14:43:06 +0700

    Update layout.tsx

--- app/berita/[id]/layout.tsx
+++ app/berita/[id]/layout.tsx
@@ -1,9 +1,1 @@
-import { ReactNode } from "react";
 
-export default function BeritaDetailLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="min-h-screen bg-[#0f1a2b] text-white">
-      {children}
-    </div>
-  );
-}
